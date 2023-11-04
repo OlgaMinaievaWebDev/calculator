4c420b89fb71aeb4d9f576ca08fa39d16f375140
Olga <minaeva9@gmail.com>
2023-11-04 15:56:29 -0400

    addEventListener to each number, add function calculate

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("hi!");
+const userInput = document.querySelector(".userInput");
+
+const numbers = document.querySelectorAll(".box");
+
+let calculation = [];
+let accumulativeNumbers;
+
+function calculate(number) {
+  const value = number.textContent;
+  calculation.push(value);
+  accumulativeNumbers = calculation.join("");
+  userInput.textContent = accumulativeNumbers;
+  console.log(calculation);
+}
+
+numbers.forEach((number) =>
+  number.addEventListener("click", () => calculate(number))
+);
