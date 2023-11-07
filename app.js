c0e73b8b706b8ae464c620249a4bebf342aa8702
Olga <minaeva9@gmail.com>
2023-11-07 16:18:03 -0500

    reset function added

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
+const clear = document.getElementById("clear-btn");
 
+function sendNumberValue(number) {
+  let displayValue = display.textContent;
+  display.textContent = displayValue === "0" ? number : displayValue + number;
+}
+
+//add Event Listener for numbers, operators, decimal buttons
 buttons.forEach((button) => {
-  button.addEventListener("click", function (e) {
-    console.log(button.value);
-  });
+  if (button.classList.length === 0) {
+    button.addEventListener("click", function () {
+      sendNumberValue(button.value);
+    });
+  } else if (button.classList.contains("operators")) {
+    button.addEventListener("click", function () {
+      sendNumberValue(button.value);
+    });
+  } else if (button.classList.contains("decimal")) {
+    button.addEventListener("click", function () {
+      sendNumberValue();
+    });
+  }
 });
+
+//Reset display
+function resetAll() {
+  display.textContent = "0";
+}
+
+clear.addEventListener("click", resetAll);
